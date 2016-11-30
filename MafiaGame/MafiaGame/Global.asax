7470ee0846f746c0805a9d105739bfafc0da176b
frunkyfrunk <verhagen.frank@gmail.com>
2016-11-30 11:23:53 +0100

    Uploading base project

---
+++ MafiaGame/MafiaGame/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MafiaGame.MvcApplication" Language="C#" %>
---
+++ MafiaGame/MafiaGame/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MafiaGame.Startup))]
+namespace MafiaGame
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
