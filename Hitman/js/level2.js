b2962369bbb0bc24b935e45bcf70f5c2b7aa2148
Johan Bos <bosbosjohan@gmail.com>
2016-12-08 20:22:40 +0100

    Hitman Update #7
    - Level 2 WIP

--- Hitman/js/level2.js
+++ Hitman/js/level2.js
@@ -1,5 +1,5 @@
 	jQuery(document).ready(function($){
-		
+
 		//Loading scene
 		Crafty.scene("loading", function() {
 		// Load our assets
@@ -9,30 +9,30 @@
 			}, 4000);
 		});
 		
-			Crafty.background("#000");
-			
-			// Text
-			Crafty.e("2D, Canvas, Text").attr({w: 200, h: 50, x: 490, y: 100})
-				.text("MISSION")
-				.textFont({size: '22px', family: 'Arial', weight: 'bold'})
-				.textColor('white');
-			
-			// Text
-			Crafty.e("2D, Canvas, Text").attr({w: 200, h: 50, x: 325, y: 180})
-				.text("My accountant stole all my money. Take him out!")
-				.textFont({size: '20px', family: 'Arial', weight: 'bold'})
-				.textColor('white');
+		Crafty.background("#000");
+		
+		// Text
+		Crafty.e("2D, Canvas, Text").attr({w: 200, h: 50, x: 490, y: 100})
+			.text("MISSION")
+			.textFont({size: '22px', family: 'Arial', weight: 'bold'})
+			.textColor('white');
+		
+		// Text
+		Crafty.e("2D, Canvas, Text").attr({w: 200, h: 50, x: 195, y: 180})
+			.text("My crazy ex is stalking me, I can't even leave my house! Take her out will you!")
+			.textFont({size: '20px', family: 'Arial', weight: 'bold'})
+			.textColor('white');
 
 		});
 
 		//Main scene
 		Crafty.scene("main",function() {
-			Crafty.background('#FFFFFF url(../Hitman/img/bg_level1.png) no-repeat center center');
+			Crafty.background('#FFFFFF url(../Hitman/img/bg_level2.png) no-repeat center center');
 			
 			Crafty.audio.add("shoot", "../Hitman/sounds/gun.mp3");
 
 			//Wrong Guy	
-			Crafty.sprite(81, 299, '../Hitman/img/man4.png', {s1:[0,0]});
+			Crafty.sprite(81, 299, '../Hitman/img/woman3.png', {s1:[0,0]});
 			var suspect1 = Crafty.e('2D, Canvas, s1, Mouse')
 				.attr({x: 65, y: 253, w: 38, h: 120})
 				.bind('Click', function(MouseEvent){
@@ -45,10 +45,9 @@
 				});
 
 			//Right Guy
-			Crafty.sprite(81, 299, '../Hitman/img/man6.png', {s2:[0,0]});
+			Crafty.sprite(81, 299, '../Hitman/img/woman4.png', {s2:[0,0]});
 			var suspect2 = Crafty.e('2D, Canvas, s2, Mouse')
 				.attr({x: 390, y: 270, w:43, h:160})
-				
 				.bind('Click', function(MouseEvent){
 					Crafty.audio.play("shoot");					
 
@@ -58,10 +57,9 @@
 				});
 			
 			//Wrong Guy	
-			Crafty.sprite(81, 299, '../Hitman/img/woman1.png', {s3:[0,0]});
+			Crafty.sprite(81, 299, '../Hitman/img/man3.png', {s3:[0,0]});
 			var suspect3 = Crafty.e('2D, Canvas, s3, Mouse')
 				.attr({x: 910, y: 320, w: 90, h: 330})
-				
 				.bind('Click', function(MouseEvent){
 					Crafty.audio.play("shoot");
 					
@@ -69,11 +67,7 @@
 						Crafty.scene("fail");
 					}, 1600);
 				});
-				
-			var car = Crafty.e("2D, DOM, Image")
-				.image("../Hitman/img/car.png")
-				.attr({x: 210, y: 325});
-
+	
 		});
 		
 		//Succes scene
@@ -86,10 +80,7 @@
 				.text("MISSION PASSED")
 				.textFont({size: '24px', family: 'Arial', weight: 'bold'})
 				.textColor('white');
-				
-				setTimeout(function() { 
-					window.location = "../Hitman/Hitman_2.html"
-				}, 2000);		
+		
 		});
 		
 		//Fail scene
@@ -102,10 +93,7 @@
 				.text("MISSION FAILED")
 				.textFont({size: '24px', family: 'Arial', weight: 'bold'})
 				.textColor('white');
-				
-				setTimeout(function() { 
-					window.location = "../Hitman/Hitman_2.html"
-				}, 2000);
+		
 		});
 
 		
