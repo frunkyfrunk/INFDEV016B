6d134865f3c37a1b3613ddedfee27a369664a8cb
killerMert <mert_3377@hotmail.com>
2016-12-09 01:24:31 +0100

    Login + Register complete

--- app.js
+++ app.js
@@ -2,86 +2,120 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var url = 'mongodb://localhost:27017/DEV016B';
+var passwordHolder;
+var usernameHolder;
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
 	res.sendFile(__dirname + '/client/index.html')
 })
-app.use('/client',express.static(__dirname + '/client'));
+app.use('/client', express.static(__dirname + '/client'));
 
 http.listen(3000, function () {
 	console.log("Server started");
 })
 
-io.on('connection', function(socket){
-  console.log('a user connected');
-  // socket.id = Math.random();
-  // SOCKET_LIST[socket.id] = socket;
-  
-  socket.on('signInData',function(data){
-	  if(data.username == "mert"){
-		socket.emit('signInResponse',{success:true});
-		useDatabase(searchUserDetails(data));	
-	  } else {
-		socket.emit('signInResponse',{success:false});
-	  }
-	  
-  });
-  
-  socket.on('registerData', function(data){
-	useDatabase(insertUserDetails(data));	
-  });
-  
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-  
+io.on('connection', function (socket) {
+	console.log('a user connected');
+	// socket.id = Math.random();
+	// SOCKET_LIST[socket.id] = socket;
+
+	socket.on('signInData', function (data) {
+		if (isPasswordValid(data)) {
+			socket.emit('signInResponse', {
+				success: true
+			});
+		} else {
+			socket.emit('signInResponse', {
+				success: false
+			});
+		}
+
+	});
+
+	socket.on('registerData', function (data) {
+		if (isUsernameAvailable(data)) {
+			socket.emit('registerResponse', {success: true});
+			//useDatabase(insertUserDetails(data));
+
+		} else {
+			socket.emit('registerResponse', {success: false});
+		}
+	});
+
+	socket.on('disconnect', function () {
+		console.log('user disconnected');
+	});
+
 });
 
-
-var MongoClient = require('mongodb').MongoClient
-  , assert = require('assert');
-  // Connection URL
-var url = 'mongodb://localhost:27017/DEV016B';
 // Use connect method to connect to the server
-
-
 function useDatabase(mongoQuery) {
-	MongoClient.connect(url, function(err, db) {
+	MongoClient.connect(url, function (err, db) {
 		if (err) {
 			console.log('Unable to connect to the mongoDB server. Error:', err);
-		}
-		else{
+		} else {
 			console.log("Connected to the mongoDB server");
-		mongoQuery(db,function(){			
-			db.close();
-			console.log("Disconnected from the mongoDB server");
-		});	
-		}			
-})};
+			mongoQuery(db, function () {
+				db.close();
+				console.log("Disconnected from the mongoDB server");
+			});
+		}
+	})
+};
 
-function insertUserDetails(data){
-	var insertDocument = function(db, callback) {
+function insertUserDetails(data) {
+	var insertDocument = function (db, callback) {
 		db.collection('user').insertOne({
-			"username" : data.username,
-			"password" : data.password
-		},function(err, result) {
+			"username": data.username,
+			"password": data.password
+		}, function (err, result) {
 			assert.equal(err, null);
 			console.log("Inserted a user into the user collection.");
 			callback();
-			});
-		};
+		});
+	};
 	return insertDocument;
 };
 
-function searchUserDetails(data){
-	var findUser = function(db, callback) {
-		var cursor = db.collection('user').find( { "username": "Frunk" },
-		function(err, result) {
+function searchUserDetails(data) {
+	var findDocuments = function (db, callback) {
+		db.collection('user').find({
+			"username": data.username
+		})
+		.toArray(function (err, docs) {
 			assert.equal(err, null);
-			callback();
-			});
-			console.log(cursor.username);
-		};
-		
-		return findUser;
+			if (typeof docs[0] !== 'undefined') {
+				usernameHolder = docs[0]["username"];
+				passwordHolder = docs[0]["password"];	
+			}
+			callback(docs);
+			
+		});
+	};
+	return findDocuments;
+};
+
+function isPasswordValid(data) {
+	useDatabase(searchUserDetails(data));
+	if (data.password == passwordHolder) {
+		console.log("Password is correct");
+		return true;
+	} else {
+		console.log("Password is incorrect")
+		return false;
+	};
+};
+
+function isUsernameAvailable(data) {
+	useDatabase(searchUserDetails(data));
+	if (data.username == usernameHolder) {
+		console.log("Username is unavailable");
+		return false;
+	} else {
+		console.log("Username is avialable")
+		return true;
+	};
 };
