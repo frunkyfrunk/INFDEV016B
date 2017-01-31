3ae1ef8bdb9107f50a887f61f3d0b21f0a7f5bcd
frunkyfrunk <verhagen.frank@gmail.com>
2017-01-31 18:10:13 +0100

    test

--- test/test.js
+++ test/test.js
@@ -140,35 +140,7 @@ describe('Database Tests', function() {
     
     var fs=require('fs');
     
-    describe('Check if Script files are in /js folder', function() {
-  describe('Check crafty.js', function() {
-    it('Should find crafty.js', function(done) {
-      fs.exists('./public/javascripts/crafty.js',function(exists){
-    if(exists){
-	    done();
-    }
-});
-    });
-  });
-  describe('Check game.js', function() {
-    it('Should find game.js', function(done) {
-      fs.exists('./public/javascripts/game.js',function(exists){
-    if(exists){
-	    done();
-    }
-});
-    });
-  });
-  describe('Check bankoverval.js', function() {
-    it('Should find crafty-min.js', function(done) {
-      fs.exists('./public/javascripts/bankoverval.js',function(exists){
-    if(exists){
-	    done();
-    }
-});
-    });
-  });
-});
+    
 });
 
 
